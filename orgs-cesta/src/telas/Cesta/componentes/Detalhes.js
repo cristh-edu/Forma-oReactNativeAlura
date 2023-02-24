accf7cc6ac6370296931d7b0650175906d21637d
Cristhian Eduardo <contato.cristhian.slopes@gmail.com>
2023-02-24 14:42:00 -0300

    Finalização do primeiro curso

--- orgs-cesta/src/telas/Cesta/componentes/Detalhes.js
+++ orgs-cesta/src/telas/Cesta/componentes/Detalhes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View, TouchableOpacity } from "react-native";
 
 import Texto from "../../../componentes/Texto";
 
@@ -9,6 +9,7 @@ export default function Detalhes({
   nomeFazenda,
   descricao,
   preco,
+  botao,
 }) {
   return (
     <>
@@ -19,6 +20,10 @@ export default function Detalhes({
       </View>
       <Texto style={estilos.descricao}>{descricao}</Texto>
       <Texto style={estilos.preco}>{preco}</Texto>
+
+      <TouchableOpacity style={estilos.botao} onPress={() => {}}>
+        <Texto style={estilos.textoBotao}>{botao}</Texto>
+      </TouchableOpacity>
     </>
   );
 }
@@ -54,5 +59,18 @@ const estilos = StyleSheet.create({
     fontSize: 26,
     lineHeight: 42,
     marginTop: 8,
+  },
+  botao: {
+    marginTop: 16,
+    backgroundColor: "#2A9F85",
+    paddingVertical: 16,
+    borderRadius: 6,
+  },
+  textoBotao: {
+    textAlign: "center",
+    color: "#ffffff",
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: "bold",
   },
 });
